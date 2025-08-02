072a2e60b3c69b4cb8d23bce8fd0cea1b19006e8
khayattullah73 <khayattullah73@gmail.com>
2025-08-02 17:48:29 +0800

    metadataBase: new URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hi, I'm Nahrul K",
   description: "I am confident that my skills and professional background are an excellent match for your team. You can find more details in my profile.",
+  metadataBase: new URL("https://nahrulk.pw"),
   openGraph :{
     images : `${localImagesList.cover}`,
   },
